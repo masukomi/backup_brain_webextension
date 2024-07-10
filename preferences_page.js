31a591b9bdc5919264720f947fdf17c2786ea4c2
masukomi <masukomi@masukomi.org>
2024-07-10 12:37:43 -0400

    simplified preferences page to match chrome version

--- preferences_page.js
+++ preferences_page.js
@@ -1,8 +1,6 @@
-//import {Preferences} from './preferences.js'
-
 async function restorePrefs(){
     // const bb_url = await Preferences.get('backup_brain_url')
-    let storage = await chrome.storage.sync.get('backup_brain_url')
+    let storage = await browser.storage.local.get('backup_brain_url')
     let bb_url = storage.backup_brain_url
     if (bb_url) {
         if (bb_url.match(/\/$/)) {
@@ -10,22 +8,19 @@ async function restorePrefs(){
         }
         document.getElementById('backup_brain_url').value = bb_url;
     }
-    storage = await chrome.storage.sync.get('show_notifications')
+    storage = await browser.storage.local.get('show_notifications')
     let sn = storage.show_notifications
     if (sn === undefined || sn === null) {
         sn = true;
     }
 
     document.getElementById('show_notifications').checked = sn;
-
-
-
 }
 async function savePrefs(){
     let new_bb_url=document.getElementById('backup_brain_url').value;
     let new_values = {}
     if (new_bb_url) {
-        new_values['backu_brain_url'] = new_bb_url
+        new_values['backup_brain_url'] = new_bb_url
     } else {
         document.getElementById('feedback').innerHTML="<span style='color: red'>Please enter the url of your Backup Brain</span>"
 
@@ -34,12 +29,14 @@ async function savePrefs(){
     let show_notifications=document.getElementById('show_notifications').checked;
     new_values['show_notifications'] = show_notifications;
 
-    await chrome.storage.sync.set(new_values);
+    await browser.storage.local.set(new_values);
+
+
+    let stored_url = await browser.storage.local.get('backup_brain_url')
 
     if (new_bb_url){
         document.getElementById('feedback').innerHTML="<span style='color: green'>Updated</span>"
     }
-
 }
 
 
